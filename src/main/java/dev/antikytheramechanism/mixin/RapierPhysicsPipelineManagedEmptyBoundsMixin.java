50cd6fc022758a6b67c7687ba0ea871a0e412c27
Wachii <dani.alvarado.sosa@gmail.com>
2026-08-12 13:09:43 +0200

    Use runtime Rapier mixin target

--- src/main/java/dev/antikytheramechanism/mixin/RapierPhysicsPipelineManagedEmptyBoundsMixin.java
+++ src/main/java/dev/antikytheramechanism/mixin/RapierPhysicsPipelineManagedEmptyBoundsMixin.java
@@ -4,13 +4,15 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import dev.antikytheramechanism.sublevel.ManagedRapierBounds;
-import dev.ryanhcode.sable.physics.impl.rapier.RapierPhysicsPipeline;
 import dev.ryanhcode.sable.sublevel.ServerSubLevel;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 /** Prevents Sable's EMPTY integer sentinel from being forwarded to Rapier as real local bounds. */
-@Mixin(value = RapierPhysicsPipeline.class, priority = 2000, remap = false)
+@Mixin(
+        targets = "dev.ryanhcode.sable.physics.impl.rapier.RapierPhysicsPipeline",
+        priority = 2000,
+        remap = false)
 abstract class RapierPhysicsPipelineManagedEmptyBoundsMixin {
     @WrapOperation(
             method = "onStatsChanged",
