d5f5bbeef70ddc19827525bdbf1046d123b06ce2
Wachii <dani.alvarado.sosa@gmail.com>
2026-08-12 18:01:44 +0200

    Expose explicit terrain debris detachment

--- src/main/java/dev/antikytheramechanism/client/ManagedTerrainParticleState.java
+++ src/main/java/dev/antikytheramechanism/client/ManagedTerrainParticleState.java
@@ -3,4 +3,7 @@ package dev.antikytheramechanism.client;
 /** Marker exposed by the TerrainParticle mixin once mini debris has been projected into world space. */
 public interface ManagedTerrainParticleState {
     boolean antikytheramechanism$isDetachedFromSubLevel();
+
+    /** Marks an already world-space terrain particle as permanently detached from Sable tracking. */
+    void antikytheramechanism$markDetachedFromSubLevel();
 }
