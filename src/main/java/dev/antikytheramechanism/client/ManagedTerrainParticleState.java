478d7369b0e6aa4b5eda8089e6c8d7a8b47244e8
Wachii <dani.alvarado.sosa@gmail.com>
2026-08-12 16:12:23 +0200

    Add detached terrain particle marker

---
+++ src/main/java/dev/antikytheramechanism/client/ManagedTerrainParticleState.java
@@ -0,0 +1,6 @@
+package dev.antikytheramechanism.client;
+
+/** Marker exposed by the TerrainParticle mixin once mini debris has been projected into world space. */
+public interface ManagedTerrainParticleState {
+    boolean antikytheramechanism$isDetachedFromSubLevel();
+}
