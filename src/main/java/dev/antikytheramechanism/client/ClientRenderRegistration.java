001d135d41f749f45d1848ecfd16ed19f4f241fe
Wachii <dani.alvarado.sosa@gmail.com>
2026-08-14 00:15:35 +0200

    Register frame orientation renderer

---
+++ src/main/java/dev/antikytheramechanism/client/ClientRenderRegistration.java
@@ -0,0 +1,20 @@
+package dev.antikytheramechanism.client;
+
+import dev.antikytheramechanism.AntikytheraMechanism;
+import dev.antikytheramechanism.registry.ModRegistries;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+
+@EventBusSubscriber(modid = AntikytheraMechanism.MOD_ID, value = Dist.CLIENT)
+public final class ClientRenderRegistration {
+    private ClientRenderRegistration() {}
+
+    @SubscribeEvent
+    public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerBlockEntityRenderer(
+                ModRegistries.MECHANISM_FRAME_BLOCK_ENTITY.get(),
+                MechanismFrameOrientationRenderer::new);
+    }
+}
