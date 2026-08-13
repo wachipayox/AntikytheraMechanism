5a9b3b9e1d42cfbe45eee5f0af02da55ffc0984f
Wachii <dani.alvarado.sosa@gmail.com>
2026-08-13 23:43:52 +0200

    Recognize docked oriented assemblies

--- src/main/java/dev/antikytheramechanism/assembly/AssemblyOrientationMath.java
+++ src/main/java/dev/antikytheramechanism/assembly/AssemblyOrientationMath.java
@@ -22,6 +22,22 @@ public final class AssemblyOrientationMath {
         if (Math.abs(a.anchorX() + delta.getX() - b.anchorX()) > epsilon
                 || Math.abs(a.anchorY() + delta.getY() - b.anchorY()) > epsilon
                 || Math.abs(a.anchorZ() + delta.getZ() - b.anchorZ()) > epsilon) return false;
+        return sameOrientation(a, b, epsilon);
+    }
+
+    public static boolean isDocked(MechanismAssembly assembly, double epsilon) {
+        AssemblyPose pose = assembly.poseTarget();
+        BlockPos origin = assembly.origin();
+        if (Math.abs(pose.anchorX() - (origin.getX() + .5)) > epsilon
+                || Math.abs(pose.anchorY() - (origin.getY() + .5)) > epsilon
+                || Math.abs(pose.anchorZ() - (origin.getZ() + .5)) > epsilon) return false;
+        Quaterniond expected = assembly.orientation().quaternion(new Quaterniond());
+        double dot = pose.quaternionX() * expected.x + pose.quaternionY() * expected.y
+                + pose.quaternionZ() * expected.z + pose.quaternionW() * expected.w;
+        return Math.abs(Math.abs(dot) - 1.0) <= epsilon;
+    }
+
+    private static boolean sameOrientation(AssemblyPose a, AssemblyPose b, double epsilon) {
         double dot = a.quaternionX() * b.quaternionX() + a.quaternionY() * b.quaternionY()
                 + a.quaternionZ() * b.quaternionZ() + a.quaternionW() * b.quaternionW();
         return Math.abs(Math.abs(dot) - 1.0) <= epsilon;
