ef3ef1ff80277035963933148d81d79f48fd16b9
Wachii <dani.alvarado.sosa@gmail.com>
2026-08-17 12:00:53 +0200

    Expose server chunk map for plot publication

---
+++ src/main/java/dev/antikytheramechanism/mixin/ServerChunkCacheAccessor.java
@@ -0,0 +1,12 @@
+package dev.antikytheramechanism.mixin;
+
+import net.minecraft.server.level.ChunkMap;
+import net.minecraft.server.level.ServerChunkCache;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(ServerChunkCache.class)
+public interface ServerChunkCacheAccessor {
+    @Accessor("chunkMap")
+    ChunkMap antikytheramechanism$getChunkMap();
+}
