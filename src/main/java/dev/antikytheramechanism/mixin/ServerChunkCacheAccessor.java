a48bd2c66b21face8b8338406bdb31cbd9da289d
Wachii <dani.alvarado.sosa@gmail.com>
2026-08-17 12:10:06 +0200

    Expose server chunk cache invalidation

--- src/main/java/dev/antikytheramechanism/mixin/ServerChunkCacheAccessor.java
+++ src/main/java/dev/antikytheramechanism/mixin/ServerChunkCacheAccessor.java
@@ -1,12 +1,11 @@
 package dev.antikytheramechanism.mixin;
 
-import net.minecraft.server.level.ChunkMap;
 import net.minecraft.server.level.ServerChunkCache;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(ServerChunkCache.class)
 public interface ServerChunkCacheAccessor {
-    @Accessor("chunkMap")
-    ChunkMap antikytheramechanism$getChunkMap();
+    @Invoker("clearCache")
+    void antikytheramechanism$clearCache();
 }
