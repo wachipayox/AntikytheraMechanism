9102c3615d8e17cee6d607d6da6271bb88332f42
Wachii <dani.alvarado.sosa@gmail.com>
2026-08-17 12:00:30 +0200

    Expose synchronous chunk-map promotion

---
+++ src/main/java/dev/antikytheramechanism/mixin/ChunkMapAccessor.java
@@ -0,0 +1,11 @@
+package dev.antikytheramechanism.mixin;
+
+import net.minecraft.server.level.ChunkMap;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(ChunkMap.class)
+public interface ChunkMapAccessor {
+    @Invoker("promoteChunkMap")
+    boolean antikytheramechanism$promoteChunkMap();
+}
