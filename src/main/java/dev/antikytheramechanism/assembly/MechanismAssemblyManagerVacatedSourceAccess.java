555ddfce66ff55b8846fd318b861a1a2e5b87482
Wachii <dani.alvarado.sosa@gmail.com>
2026-08-19 19:23:50 +0200

    Remove unused vacated-source access bridge

--- src/main/java/dev/antikytheramechanism/assembly/MechanismAssemblyManagerVacatedSourceAccess.java
+++
@@ -1,8 +0,0 @@
-package dev.antikytheramechanism.assembly;
-
-import net.minecraft.core.BlockPos;
-
-/** Internal mixin bridge for handing a physically vacated Create source to a new Frame. */
-public interface MechanismAssemblyManagerVacatedSourceAccess {
-    boolean antikytheramechanism$releaseContraptionSourceFrame(BlockPos framePos);
-}
