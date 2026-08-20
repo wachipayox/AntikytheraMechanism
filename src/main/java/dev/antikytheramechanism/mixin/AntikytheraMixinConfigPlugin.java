b94f394628f1b35821d2956713059a3b3cf7b9d1
Wachii <dani.alvarado.sosa@gmail.com>
2026-08-20 15:54:58 +0200

    Test Offroad suspension-only wheel forces

--- src/main/java/dev/antikytheramechanism/mixin/AntikytheraMixinConfigPlugin.java
+++ src/main/java/dev/antikytheramechanism/mixin/AntikytheraMixinConfigPlugin.java
@@ -76,7 +76,7 @@ public final class AntikytheraMixinConfigPlugin implements IMixinConfigPlugin {
         if (mixinClassName.endsWith(".PlaceMergingGluePacketAntikytheraMixin")) {
             return loader.getResource(SIM_MERGING_GLUE_PACKET) != null;
         }
-        if (mixinClassName.endsWith(".OffroadWheelMountExperimentalLateralGripMixin")) {
+        if (mixinClassName.endsWith(".OffroadWheelMountExperimentalSuspensionOnlyMixin")) {
             return loader.getResource(OFFROAD_WHEEL_MOUNT) != null;
         }
         return true;
